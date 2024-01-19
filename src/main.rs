5c15f4f78604eec6a0491bd63e7a258cccd84d94
Mirko Basic <bejzik8@gmail.com>
2024-01-19 02:12:33 +0100

    Added Axum to the project. 🍀

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-  println!("Hello, world!");
+use axum::{Router, response::Html, routing::get};
+
+#[tokio::main]
+async fn main() {
+  let routes_hello = Router::new().route(
+    "/hello",
+    get(|| async { Html("<h1>Hello, world BREEE!</h1>")})
+  );
+
+  let address = "127.0.0.1:8080";
+
+  let listener = tokio::net::TcpListener::bind(address).await.unwrap();
+  
+  println!("--> LISTENING on {address}...");
+
+  axum::serve(listener, routes_hello).await.unwrap();
 }
