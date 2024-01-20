04210c1a48c456aaeee31818f6c5319a112625bc
Mirko Basic <bejzik8@gmail.com>
2024-01-20 05:07:55 +0100

    Code formatted. 🐟

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,40 @@
-use axum::{Router, response::{Html, IntoResponse}, routing::get, extract::{Query, Path}};
+use axum::{
+    extract::{Path, Query},
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
 use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
-  let routes_hello = Router::new()
-  .route(
-    "/hello",
-    get(handler_hello)
-  ).route(
-    "/hello/:name",
-    get(handler_hello2)
-  );
+    let routes_hello = Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello/:name", get(handler_hello2));
 
-  let address = "127.0.0.1:8080";
+    let address = "127.0.0.1:8080";
 
-  let listener = tokio::net::TcpListener::bind(address).await.unwrap();
-  
-  println!("--> LISTENING on {address}...");
+    let listener = tokio::net::TcpListener::bind(address).await.unwrap();
 
-  axum::serve(listener, routes_hello).await.unwrap();
+    println!("--> LISTENING on {address}...");
+
+    axum::serve(listener, routes_hello).await.unwrap();
 }
 
-#[derive(Debug, Deserialize)]  
+#[derive(Debug, Deserialize)]
 struct HelloParams {
-  name: Option<String>
+    name: Option<String>,
 }
 
 async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
-  print!("--> {:<12} - handler_hello - {params:?}", "HANDLER");
+    print!("--> {:<12} - handler_hello - {params:?}", "HANDLER");
 
-  let name = params.name.as_deref().unwrap_or("World!");
-  Html(format!("Hello <strong>{name}</strong>!"))
+    let name = params.name.as_deref().unwrap_or("World!");
+    Html(format!("Hello <strong>{name}</strong>!"))
 }
 
 async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
-  print!("--> {:<12} - handler_hello2 - {name:?}", "HANDLER");
+    print!("--> {:<12} - handler_hello2 - {name:?}", "HANDLER");
 
-  Html(format!("Hello2 <strong>{name}</strong>!"))
+    Html(format!("Hello2 <strong>{name}</strong>!"))
 }
