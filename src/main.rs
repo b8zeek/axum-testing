2386066c2534ae039925caf97ee20da7312a7717
Mirko Basic <bejzik8@gmail.com>
2024-01-20 10:33:43 +0100

    Created login fail custom error. 🐔

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+pub use self::error::{Error, Result};
+
 use axum::{
     extract::{Path, Query},
     response::{Html, IntoResponse},
@@ -6,6 +8,8 @@ use axum::{
 };
 use serde::Deserialize;
 use tower_http::services::ServeDir;
+
+mod error;
 
 #[tokio::main]
 async fn main() {
