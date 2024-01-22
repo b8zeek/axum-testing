e67319816eeac6e983cf4d28b50b0eb79cf2b1b1
Mirko Basic <bejzik8@gmail.com>
2024-01-22 01:11:21 +0100

    Created parse_token function. 🐼

--- src/web/mw_auth.rs
+++ src/web/mw_auth.rs
@@ -1,4 +1,5 @@
 use axum::{body::Body, extract::Request, middleware::Next, response::Response};
+use lazy_regex::regex_captures;
 use tower_cookies::Cookies;
 
 use crate::{web::AUTH_TOKEN, Error, Result};
@@ -6,7 +7,20 @@ use crate::{web::AUTH_TOKEN, Error, Result};
 pub async fn mw_require_auth(cookies: Cookies, req: Request<Body>, next: Next) -> Result<Response> {
     let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
 
-    auth_token.ok_or(Error::AuthFailNoAuthTokenCookie)?;
+    let (user_id, exp, sign) = auth_token
+        .ok_or(Error::AuthFailNoAuthTokenCookie)
+        .and_then(parse_token)?;
 
     Ok(next.run(req).await)
+}
+
+fn parse_token(token: String) -> Result<(u64, String, String)> {
+    let (_while, user_id, exp, sign) = regex_captures!(r#"^user-(\d+)\.(.+)\.(.+)"#, &token)
+        .ok_or(Error::AuthFailTokenWrongFormat)?;
+
+    let user_id: u64 = user_id
+        .parse()
+        .map_err(|_| Error::AuthFailTokenWrongFormat)?;
+
+    Ok((user_id, exp.to_string(), sign.to_string()))
 }
